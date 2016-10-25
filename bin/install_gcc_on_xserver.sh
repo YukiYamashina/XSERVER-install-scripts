45e7137209eded363a7bae3317d5eda31ceee202
Yuki Yamashina <yuki@MBA-13inch.local>
2016-10-26 06:25:45 +0900

    first commit

---
+++ bin/install_gcc_on_xserver.sh
@@ -0,0 +1,265 @@
+#!/bin/bash
+#
+#
+# curl -o- https://gist.githubusercontent.com/YukiYamashina/2b163e5b18b755200e5e1644b8074fc0/raw/install_gcc_on_xserver.sh | bash
+#
+#
+#----------------------------------------------------------------------
+#
+install_dir=$HOME/opt
+#
+#----------------------------------------------------------------------
+#
+echo "Install to ${install_dir}"
+cd $HOME
+mkdir -p ${install_dir}/src
+
+export LD_LIBRARY_PATH=${install_dir}/lib
+export LD_RUN_PATH=${install_dir}/lib
+
+#
+#----------------------------------------------------------------------
+#
+resource=xz
+version=5.2.2
+target=bin/xz
+resource_uri="http://tukaani.org/xz/${resource}-${version}.tar.gz"
+configure_options="--prefix=${install_dir} --with-gmp=${install_dir}"
+#
+# http://qiita.com/sirone/items/511c6f68d763dbb66e14
+# http://www.mpfr.org/
+#
+#----------------------------------------------------------------------
+#
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Installing ${resource}-${version}..."
+    cd ${install_dir}/src
+    mkdir ${resource}
+    wget ${resource_uri} -O ${resource}.tar.gz
+    tar -xvzf ${resource}.tar.gz -C ${install_dir}/src/${resource} --strip-components=1
+    cd ${resource}
+    mkdir build; cd build
+    ../configure ${configure_options}
+    make check; make install
+    cd ${install_dir}/src
+    rm -rf ${resource}*
+else
+    echo "${resource} already installed..."
+fi
+
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Failed to install ${resource}, exit..."
+    exit
+fi
+
+#
+#----------------------------------------------------------------------
+#
+resource=gmp
+version=6.1.1
+target=lib/libgmp.a
+resource_uri="https://ftp.gnu.org/gnu/gmp/${resource}-${version}.tar.xz"
+configure_options="--prefix=${install_dir} --enable-cxx"
+#
+# http://qiita.com/sirone/items/511c6f68d763dbb66e14
+# https://gmplib.org/
+#
+#----------------------------------------------------------------------
+#
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Installing ${resource}-${version}..."
+    cd ${install_dir}/src
+    mkdir ${resource}
+    wget ${resource_uri} -O ${resource}.tar.xz
+    unxz ${resource}.tar.xz
+    tar -xf ${resource}.tar -C ${install_dir}/src/${resource} --strip-components=1
+    cd ${resource}
+    mkdir build; cd build
+    ../configure ${configure_options}
+    make -s -j2; make check -s -j2; make install
+    cd ${install_dir}/src
+    rm -rf ${resource}*
+else
+    echo "${resource} already installed..."
+fi
+
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Failed to install ${resource}, exit..."
+    exit
+fi
+
+#
+#----------------------------------------------------------------------
+#
+resource=mpfr
+version=3.1.5
+target=lib/libmpfr.a
+resource_uri="http://www.mpfr.org/mpfr-current/${resource}-${version}.tar.gz"
+configure_options="--prefix=${install_dir} --with-gmp=${install_dir}"
+#
+# http://qiita.com/sirone/items/511c6f68d763dbb66e14
+# http://www.mpfr.org/
+#
+#----------------------------------------------------------------------
+#
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Installing ${resource}-${version}..."
+    cd ${install_dir}/src
+    mkdir ${resource}
+    wget ${resource_uri} -O ${resource}.tar.gz
+    tar -xvzf ${resource}.tar.gz -C ${install_dir}/src/${resource} --strip-components=1
+    cd ${resource}
+    mkdir build; cd build
+    ../configure ${configure_options}
+    make check; make install
+    cd ${install_dir}/src
+    rm -rf ${resource}*
+else
+    echo "${resource} already installed..."
+fi
+
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Failed to install ${resource}, exit..."
+    exit
+fi
+
+#
+#----------------------------------------------------------------------
+#
+resource=mpc
+version=1.0.3
+target=lib/libmpc.a
+resource_uri="ftp://ftp.gnu.org/gnu/mpc/${resource}-${version}.tar.gz"
+configure_options="--prefix=${install_dir} --with-gmp=${install_dir}"
+#
+# http://qiita.com/sirone/items/511c6f68d763dbb66e14
+# http://www.multiprecision.org/
+#
+#----------------------------------------------------------------------
+#
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Installing ${resource}-${version}..."
+    cd ${install_dir}/src
+    mkdir ${resource}
+    wget ${resource_uri} -O ${resource}.tar.gz
+    tar -xvzf ${resource}.tar.gz -C ${install_dir}/src/${resource} --strip-components=1
+    cd ${resource}
+    mkdir build; cd build
+    ../configure ${configure_options}
+    make check; make install
+    cd ${install_dir}/src
+    rm -rf ${resource}*
+else
+    echo "${resource} already installed..."
+fi
+
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Failed to install ${resource}, exit..."
+    exit
+fi
+
+#
+#----------------------------------------------------------------------
+#
+resource=isl
+version=0.17.1
+target=lib/libisl.a
+resource_uri="http://isl.gforge.inria.fr/${resource}-${version}.tar.gz"
+configure_options="--prefix=${install_dir} --with-gmp-prefix=${install_dir}"
+#
+# http://qiita.com/sirone/items/511c6f68d763dbb66e14
+# http://isl.gforge.inria.fr/
+# https://groups.google.com/forum/#!topic/isl-development/WqjSBN_nIeI
+#
+#----------------------------------------------------------------------
+#
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Installing ${resource}-${version}..."
+    cd ${install_dir}/src
+    mkdir ${resource}
+    wget ${resource_uri} -O ${resource}.tar.gz
+    tar -xvzf ${resource}.tar.gz -C ${install_dir}/src/${resource} --strip-components=1
+    cd ${resource}
+    mkdir build; cd build
+    ../configure ${configure_options}
+    make check; make install
+    cd ${install_dir}/src
+    rm -rf ${resource}*
+else
+    echo "${resource} already installed..."
+fi
+
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Failed to install ${resource}, exit..."
+    exit
+fi
+
+#
+#----------------------------------------------------------------------
+#
+resource=cloog
+version=0.18.4
+target=lib/libcloog-isl.a
+resource_uri="http://www.bastoul.net/cloog/pages/download/count.php3?url=./${resource}-${version}.tar.gz"
+configure_options="--prefix=${install_dir} --with-gmp-prefix=${install_dir}"
+#
+# http://qiita.com/sirone/items/511c6f68d763dbb66e14
+# http://www.cloog.org/
+#
+#----------------------------------------------------------------------
+#
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Installing ${resource}-${version}..."
+    cd ${install_dir}/src
+    mkdir ${resource}
+    wget ${resource_uri} -O ${resource}.tar.gz
+    tar -xvzf ${resource}.tar.gz -C ${install_dir}/src/${resource} --strip-components=1
+    cd ${resource}
+    mkdir build; cd build
+    ../configure ${configure_options}
+    make check; make install
+    cd ${install_dir}/src
+    rm -rf ${resource}*
+else
+    echo "${resource} already installed..."
+fi
+
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Failed to install ${resource}, exit..."
+    exit
+fi
+
+#
+#----------------------------------------------------------------------
+#
+resource=gcc
+version=4.9.4
+target=bin/gcc
+resource_uri="http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/${resource}-${version}/${resource}-${version}.tar.gz"
+configure_options="--disable-multilib --enable-languages=c,c++,java --program-suffix=-${version} --prefix=${install_dir} --with-gmp=${install_dir} --with-mpfr=${install_dir} --with-mpc=${install_dir}"
+#
+# http://qiita.com/sirone/items/511c6f68d763dbb66e14
+# https://www.gnu.org/software/gcc/
+#
+#----------------------------------------------------------------------
+#
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Installing ${resource}-${version}..."
+    cd ${install_dir}/src
+    mkdir ${resource}
+    wget ${resource_uri} -O ${resource}.tar.gz
+    tar -xvzf ${resource}.tar.gz -C ${install_dir}/src/${resource} --strip-components=1
+    cd ${resource}
+    mkdir build; cd build
+    ../configure ${configure_options}
+    make; make install
+    cd ${install_dir}/src
+    rm -rf ${resource}*
+else
+    echo "${resource} already installed..."
+fi
+
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Failed to install ${resource}, exit..."
+    exit
+fi
---
+++ bin/install_git_on_xserver.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+#
+#
+# curl -o- https://gist.githubusercontent.com/YukiYamashina/49a57969890866341c6cca7e62bfacea/raw/install_git_on_xserver.sh | bash
+#
+#
+#----------------------------------------------------------------------
+#
+install_dir=$HOME/opt
+#
+#----------------------------------------------------------------------
+#
+echo "Install to ${install_dir}"
+cd $HOME
+mkdir -p ${install_dir}/src
+
+#
+#----------------------------------------------------------------------
+#
+resource=gettext
+version=0.19.8
+target=bin/gettext
+resource_uri="http://ftp.gnu.org/pub/gnu/gettext/${resource}-${version}.tar.gz"
+configure_options="--prefix=${install_dir}"
+#
+#----------------------------------------------------------------------
+#
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Installing ${resource}-${version} for git..."
+    cd ${install_dir}/src
+    mkdir ${resource}
+    wget ${resource_uri} -O ${resource}.tar.gz
+    tar -xvzf ${resource}.tar.gz -C ${install_dir}/src/${resource} --strip-components=1
+    cd ${resource}
+    mkdir build; cd build
+    ../configure ${configure_options}
+    make; make install
+    cd ${install_dir}/src
+    rm -rf ${resource}*
+else
+    echo "${resource} already installed..."
+fi
+
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Failed to install ${resource}, exit..."
+    exit
+fi
+
+#
+#----------------------------------------------------------------------
+#
+resource=curl
+version=7.50.3
+target=bin/curl
+resource_uri="http://curl.askapache.com/download/${resource}-${version}.tar.gz"
+configure_options="--prefix=${install_dir} --enable-libcurl-option"
+#
+#----------------------------------------------------------------------
+#
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Installing ${resource}-${version} for git..."
+    cd ${install_dir}/src
+    mkdir ${resource}
+    wget ${resource_uri} -O ${resource}.tar.gz
+    tar -xvzf ${resource}.tar.gz -C ${install_dir}/src/${resource} --strip-components=1
+    cd ${resource}
+    mkdir build; cd build
+    ../configure ${configure_options}
+    make; make install
+    cd ${install_dir}/src
+    rm -rf ${resource}*
+else
+    echo "${resource} already installed..."
+fi
+
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Failed to install ${resource}, exit..."
+    exit
+fi
+
+#
+#----------------------------------------------------------------------
+#
+resource=git
+target=bin/git
+configure_options="--prefix=${install_dir} --with-curl=${install_dir} --with-expat==${install_dir}"
+#
+#----------------------------------------------------------------------
+#
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Installing ${resource}..."
+    cd ${install_dir}/src
+    wget https://github.com/git/git/archive/master.zip
+    unzip master.zip
+    cd git-master
+    autoconf
+    ./configure ${configure_options} CFLAGS="-I${install_dir}/include"
+    make -i all
+    make -i install
+    cd ${install_dir}/src
+    rm -rf git-master master.zip
+    cd $HOME
+else
+    echo "${resource} already installed..."
+fi
+
+if [ ! -e ${install_dir}/${target} ]; then
+    echo "Failed to install ${resource}, exit..."
+    exit
+fi
